46528fe3598e74f115b68ab1f9b049642fe0b51f
maduxt <maria.eduarda2812@hotmail.com>
2024-03-20 19:50:13 -0300

    feat app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header/header';
+import './global.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
