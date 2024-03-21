2cab622788dd53ac3395567a0927469a6fe724b5
maduxt <maria.eduarda2812@hotmail.com>
2024-03-21 18:42:11 -0300

    style app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './components/Header/header';
-import './global.css'
+import Footer from './components/Footer/footer'
+import Inicio from './pages/Inicio/inicio'
+import './reset.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Inicio />
+      <Footer />
     </div>
   );
 }
