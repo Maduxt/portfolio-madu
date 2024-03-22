1fd5c4c308e7b6d4c4cc4f5f505ccdda090c24f7
maduxt <maria.eduarda2812@hotmail.com>
2024-03-22 19:30:11 -0300

    style inicio

--- src/pages/Inicio/inicio.jsx
+++ src/pages/Inicio/inicio.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './inicio.css'
 import FirstSection from '../../components/FirstSection/FirstSection'
+import SecondSection from '../../components/SecondSection/SecondSection'
+import ThirdSection from '../../components/ThirdSection/ThirdSection'
 
 const Inicio = () => {
   return (
     <div className='inicio'>
       <FirstSection />
+      <SecondSection />
+      <ThirdSection />
     </div>
   )
 }
