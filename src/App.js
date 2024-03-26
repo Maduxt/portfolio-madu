351bf7df4c8a3eb663cbee578b74d9daed8bc6fe
maduxt <maria.eduarda2812@hotmail.com>
2024-03-26 19:23:20 -0300

    style app

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from './components/Header/header';
 import Footer from './components/Footer/footer'
 import Inicio from './pages/Inicio/inicio'
 import './reset.css'
+// import Sobre from './pages/Sobre/Sobre';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* <Sobre /> */}
       <Inicio />
       <Footer />
     </div>
