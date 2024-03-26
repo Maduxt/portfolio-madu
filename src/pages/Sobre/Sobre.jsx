7acc66914aadd8810307c8ecfeac0f81bdee492e
maduxt <maria.eduarda2812@hotmail.com>
2024-03-26 19:21:46 -0300

    feat sobre

---
+++ src/pages/Sobre/Sobre.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Sobre.css'
+
+const Sobre = () => {
+  return (
+    <section className='sobrePage'>
+        <p>
+            aaaaa
+        </p>
+    </section>
+  )
+}
+
+export default Sobre
