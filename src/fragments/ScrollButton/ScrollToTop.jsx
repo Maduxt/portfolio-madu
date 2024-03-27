1d012d1fddb95c3d25604329d1b6e0b3c230432d
maduxt <maria.eduarda2812@hotmail.com>
2024-03-27 17:20:20 -0300

    style button scroll

--- src/fragments/ScrollButton/ScrollToTop.jsx
+++ src/fragments/ScrollButton/ScrollToTop.jsx
@@ -6,7 +6,7 @@ const ScrollToTop = () => {
     const [showTopBtn, setShowTopBtn] = useState(false);
     useEffect(() => {
         window.addEventListener("scroll", () => {
-            if (window.scrollY > 700) {
+            if (window.scrollY > 520) {
                 setShowTopBtn(true);
             } else {
                 setShowTopBtn(false);
