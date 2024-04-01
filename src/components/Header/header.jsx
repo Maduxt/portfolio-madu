27de71cd7b92c4abef1c3b156bbd4bb2cca23a75
maduxt <maria.eduarda2812@hotmail.com>
2024-04-01 17:28:54 -0300

    style header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,23 +1,69 @@
 import { Link } from 'react-router-dom'
-import React from 'react'
+import React, {useState} from 'react'
 import './header.css'
 import Logo from '../../fragments/Logo/logo'
 import Button from '../../fragments/ButtonTheme/button'
+import {motion} from 'framer-motion'
+import {HiBars3} from 'react-icons/hi2'
+import { HeaderMenu } from '../Data/Data'
+import { PiSignOutLight } from "react-icons/pi"
 
 const Header = () => {
+  const [selected, setSelected] = useState(0)
+  const [expanded, setExpanded] = useState(true)
+
+  const sidebarVariants = {
+    true: {
+      left: '0'
+    },
+    false: {
+      left: '-60%'
+    }
+  }
   return (
-    <nav className='navbar'>
+    <>
+    
+    <motion.div className='navbar'
+    variants={sidebarVariants}
+    animate={window.innerWidth<=480?`${expanded}`:''}
+    >
+
+    <div className='logo'>
         <Logo />
-        <div className='navRight'>
-            <ul className='navegation'>
-                <li className='links'><Link className='link' to='/'> INICIO </Link></li>
-                <li className='links'><Link className='link' to='/sobre'> SOBRE </Link></li>
-                <li className='links'><Link className='link' to='/projetos'> PROJETOS </Link></li>
-                <li className='links'><Link className='link' to='/contato'> CONTATO </Link></li>
-            </ul>
-        <Button />
+    </div>
+
+
+        <div className='menu'>
+              {HeaderMenu.map((item, index)=>{
+                return(
+                  <div className={selected===index?'menuItem active': 'menuItem'}
+                  key={index}
+                  onClick={()=>setSelected(index)}>
+
+                    {item.heading}
+
+                  </div>
+                )
+              })}
+
+          <Button />
+
+          <div className="menuItem">
+                <PiSignOutLight />
+            </div>
+
         </div>
-    </nav>
+
+    </motion.div>
+
+    <div className='bars'
+      style={expanded? {top: '50%'}:{top: '50%'}}
+      onClick={()=>setExpanded(!expanded)}
+      >
+      <HiBars3 />
+    </div>
+
+    </>
   )
 }
 
